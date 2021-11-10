463f06564b428d434dd4aa4568d0670b351bd995
LahiaOmar <omar.lahia@gmail.com>
2021-11-10 10:00:42 +0100

    update ast test file

--- test/ast.test.ts
+++ test/ast.test.ts
@@ -1,18 +1,56 @@
-import {fileASTparser} from '../src/helpers/parser'
+import { AstParser, Component } from '../src/helpers/parser'
+import { readFile } from '../src/helpers/fs'
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toContainComponent(arrayComponent:string[]): R;
+    }
+  }
+}
+
+expect.extend({
+  toContainComponent(received:Component[], arrayComponent:string[]){
+    const notFoundComponent:string[] = []
+
+    arrayComponent.forEach(componentName => {
+      const found = received.find(({info}) => info.name === componentName)
+      if(!found){
+        notFoundComponent.push(componentName)
+      }
+    })
+    const pass = notFoundComponent.length === 0
+    
+    if(pass){
+      return{
+        message: () => `All component is founded`,
+        pass: true
+      }
+    }
+    else{
+      return {
+        message: () => `Those components are not found :${notFoundComponent}`,
+        pass: false
+      }
+    }
+  }
+})
 
 describe("TESTING AST PARSER", () => {
-  it("should create a ast tree from js code", () => {
-    const stringCode = `
-    import defImp, { Route as Routes, Switch } from './react-router-dom'
-    <>
-    <App.app />
-    <Container />
-    </>
-    const name = "omar"
-    `
-    const {importStatement, exportStatements, JSXtags} = fileASTparser(stringCode, {})
-    
-    expect(JSXtags).toContain("App")
-    expect(JSXtags).toContain("Container")
+  
+  test("should parse the 3 types of react components [FUNC, CLASS, ARROW] ", async () => {
+    const filePath = './test/mock/single-component/all-types-components.js' 
+    const fileContent = await readFile(filePath)
+    const astParser = new AstParser()
+
+    const { components } = astParser.parse(fileContent)
+
+    const expectedComponents = [
+      'ClassComponent',
+      'FunctionComponent',
+      'arrowFunctionComponent',
+    ]
+
+    expect(components).toContainComponent(expectedComponents)
   })
 })
