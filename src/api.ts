7378e6443b486aac7090278274a8eec6eb54ef85
LahiaOmar <omar.lahia@gmail.com>
2021-11-17 18:02:14 +0100

    update error message

--- src/api.ts
+++ src/api.ts
@@ -164,7 +164,7 @@ export default class Api {
 
         if (!indexPath && !packagePath) {
           throw new Error(
-            "there no index.js or package.json to resolve the component"
+            `there no index.js or package.json in this folder ${nextPath}`
           );
         }
 
