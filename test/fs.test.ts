2beb2c5d5ac502d677a7d260c26ede38412aa7b0
LahiaOmar <omar.lahia@gmail.com>
2021-11-29 11:57:02 +0100

    removing test

--- test/fs.test.ts
+++ test/fs.test.ts
@@ -1,11 +1,5 @@
-import { isValidDirectory } from "../src/helpers/fs";
+import {} from "../src/helpers/fs";
 
 describe("TESTING SOME FS HELPERS", () => {
-  it("should verify if a path is a valid directory path", async () => {
-    const path = process.cwd();
-    const toSkip = ["test", "node_modules"];
-    const isValid = await isValidDirectory(path, toSkip);
-
-    expect(isValid).toBe(true);
-  });
+  it("", () => {});
 });
