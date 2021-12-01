8b82fdd60e0d2a2b1ae786e936268ba1a4f8b4e8
LahiaOmar <omar.lahia@gmail.com>
2021-12-01 17:53:18 +0100

    add some test to fs helpers

--- test/fs.test.ts
+++ test/fs.test.ts
@@ -1,5 +1,27 @@
-import {} from "../src/helpers/fs";
+import { join, isFile, isFilePath, isDirectory } from "../src/helpers/fs";
 
 describe("TESTING SOME FS HELPERS", () => {
-  it("", () => {});
+  it("isFile should return true with a valid file path  ", async () => {
+    const pathFile = join(process.cwd(), "/src/index.ts");
+
+    const isAFile = await isFile(pathFile);
+
+    expect(isAFile).toBe(true);
+  });
+
+  it("isFilePath should return true with a valid file path and his extension", async () => {
+    const pathFile = join(process.cwd(), "/src/index");
+
+    const isAFile = await isFilePath(pathFile, ".ts");
+
+    expect(isAFile).toBe(true);
+  });
+
+  it("isDirectory should return true with valide path directory", async () => {
+    const pathDir = process.cwd();
+
+    const isDir = await isDirectory(pathDir);
+
+    expect(isDir).toBe(true);
+  });
 });
